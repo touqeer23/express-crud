0f378432cf701fb56aeaef6721505502b50dfb96
Mubashir Maqbool <mubashir.maqbool@funavry.com>
2022-09-20 18:42:42 +0500

    modified code added api for getting users and uploads.

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 const multer  = require('multer')
 const uploader = multer();
-const fileUploads = uploader.fields([{ name: 'user_profile', maxCount: 1 }, { name: 'files', maxCount: 4 }])
+const fileUploads = uploader.fields([{ name: 'user_profile', maxCount: 1 }, { name: 'files', maxCount: 1 }])
 
 
 router.post('/register',userController.register);
 router.post('/login',userController.login);
 router.post('/uploads', fileUploads, userController.uploadFiles);
+router.post('/save-upload',userController.saveUploadedFile);
+router.post('/get-user-info',userController.getUsers);
 
 module.exports = router;
