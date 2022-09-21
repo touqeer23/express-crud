31a325e112d60fafe0289e6abbc588ae98c06805
touqeer23 <111746822+touqeer23@users.noreply.github.com>
2022-09-21 19:45:56 +0500

    corrected mongo url

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -1,5 +1,5 @@
 
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://dev_user:<password>root@cluster0.vfbr8wz.mongodb.net/?retryWrites=true&w=majority').then(() => { console.log('Database connected!') }).catch((err) => { console.log('error in connecting to database',err) });
+mongoose.connect('mongodb+srv://dev_user:root@cluster0.vfbr8wz.mongodb.net/?retryWrites=true&w=majority').then(() => { console.log('Database connected!') }).catch((err) => { console.log('error in connecting to database',err) });
 
