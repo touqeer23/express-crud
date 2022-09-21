2f3d6d93c45943fcce67b9bc7eefac79caea54ff
touqeer23 <111746822+touqeer23@users.noreply.github.com>
2022-09-21 19:40:19 +0500

    updated mongo connection url

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -1,5 +1,5 @@
 
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://test_user:root@cluster0.5y1hbxf.mongodb.net/test-db?retryWrites=true&w=majority').then(() => { console.log('Database connected!') }).catch((err) => { console.log('error in connecting to database',err) });
+mongoose.connect('mongodb+srv://dev_user:<password>root@cluster0.vfbr8wz.mongodb.net/?retryWrites=true&w=majority').then(() => { console.log('Database connected!') }).catch((err) => { console.log('error in connecting to database',err) });
 
